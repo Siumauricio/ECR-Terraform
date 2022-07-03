07a7ed0ec55c3b46e8ca8276732f757565b44036
Siumauricio <siumauricio@unitec.edu>
2022-07-03 16:37:58 -0600

    refactor: defined variables of vpc module

--- enviroments/staging/main.tf
+++ enviroments/staging/main.tf
@@ -5,6 +5,12 @@ provider "aws" {
 # VPC
 #-----------------------------
 module "vpc" {
-  source     = "../../modules/vpc"
-  cidr_block = "10.0.0.0/16"
+  source                = "../../modules/vpc"
+  cidr_block            = "10.0.0.0/16"
+  public_subnet_1_cidr  = "10.0.1.0/24"
+  public_subnet_2_cidr  = "10.0.2.0/24"
+  public_subnet_3_cidr  = "10.0.3.0/24"
+  private_subnet_1_cidr = "10.0.4.0/24"
+  private_subnet_2_cidr = "10.0.5.0/24"
+  private_subnet_3_cidr = "10.0.6.0/24"
 }
