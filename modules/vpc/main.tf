210773eea311b105edc45b552cd909b306a295ed
Siumauricio <siumauricio@unitec.edu>
2022-07-03 01:11:20 -0600

    feat: added vpc module

---
+++ modules/vpc/main.tf
@@ -0,0 +1,136 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
+  tags = {
+    Name = "vpc-staging"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1" {
+  cidr_block        = var.public_subnet_1_cidr
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  cidr_block        = var.public_subnet_2_cidr
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Public-Subnet-2"
+  }
+}
+
+
+resource "aws_subnet" "public-subnet-3" {
+  cidr_block        = var.public_subnet_3_cidr
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "Public-Subnet-3"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  cidr_block        = var.private_subnet_1_cidr
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private-Subnet-3"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  cidr_block        = var.private_subnet_2_cidr
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private-Subnet-3"
+  }
+}
+
+resource "aws_subnet" "private-subnet-3" {
+  cidr_block        = var.private_subnet_3_cidr
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "Private-Subnet-3"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "Public-Route-Table"
+  }
+}
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "Private-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "public-subnet-1-association" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "public-subnet-2-association" {
+  subnet_id      = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "public-subnet-3-association" {
+  subnet_id      = aws_subnet.public-subnet-3.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+resource "aws_route_table_association" "private-subnet-1-association" {
+  subnet_id      = aws_subnet.private-subnet-1.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "private-subnet-2-association" {
+  subnet_id      = aws_subnet.private-subnet-2.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "private-subnet-3-association" {
+  subnet_id      = aws_subnet.private-subnet-3.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+resource "aws_eip" "elastic-ip-for-nat-gw" {
+  vpc                       = true
+  associate_with_private_ip = "10.0.0.5"
+  tags = {
+    Name = "Elastic-IP-for-NAT-GW"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.elastic-ip-for-nat-gw.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+  tags = {
+    Name = "NAT-GW"
+  }
+  depends_on = [
+    aws_eip.elastic-ip-for-nat-gw
+  ]
+}
+
+resource "aws_route" "nat-gw-route" {
+  route_table_id         = aws_route_table.private-route-table.id
+  nat_gateway_id         = aws_nat_gateway.nat-gw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for the public subnet 1"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for the public subnet 2"
+}
+
+variable "public_subnet_3_cidr" {
+  description = "CIDR block for the public subnet 3"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for the private subnet 1"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for the private subnet 2"
+}
+
+variable "private_subnet_3_cidr" {
+  description = "CIDR block for the private subnet 3"
+}
